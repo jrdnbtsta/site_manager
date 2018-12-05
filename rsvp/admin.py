5ac5395824d5713c0e82cd3c750cec6a8f3fe448
Jordan Bautista <jordanbau@gmail.com>
2018-12-05 14:01:31 -0800

    models setup

--- rsvp/admin.py
+++ rsvp/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from rsvp.models import Guest, Party
 
 # Register your models here.
+class GuestAdmin(admin.ModelAdmin):
+    model = Guest
+    list_display = ("first_name", "last_name", "attending_wedding", "attending_welcome_dinner", "get_party")
+
+    def get_party(self, obj):
+        party = Party.objects.filter(guest=obj).first()
+        string = "\n"
+        return "{} - {}".format(party.id, party.name)
+
+class PartyAdmin(admin.ModelAdmin):
+    model = Party
+    list_display = ("id", "name", "get_guests")
+    filter_horizontal = ('guest',)
+
+    def get_guests(self, obj):
+        return "\n".join([f"{g.first_name} {g.last_name}, " for g in obj.guest.all()])[:-2]
+
+admin.site.register(Guest, GuestAdmin)
+admin.site.register(Party, PartyAdmin)
--- rsvp/models.py
+++ rsvp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Guest(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.CharField(max_length=64, blank=True)
+    address = models.CharField(max_length=64, blank=True)
+    attending_wedding = models.BooleanField(null=True)
+    attending_welcome_dinner = models.BooleanField(null=True)
+    notes = models.CharField(max_length=64, null=True)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+class Party(models.Model):
+    name = models.CharField(max_length=64, blank=True)
+    guest = models.ManyToManyField(Guest)
+
+    def __str__(self):
+        return self.name
