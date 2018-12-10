fe394b029c1672b8133a3c583cf4ae3b42c050d0
Jordan Bautista <jordanbau@gmail.com>
2018-12-10 09:24:00 -0800

    notes not required

--- rsvp/models.py
+++ rsvp/models.py
@@ -9,7 +9,7 @@ class Guest(models.Model):
     address = models.CharField(max_length=64, blank=True)
     attending_wedding = models.BooleanField(null=True)
     attending_welcome_dinner = models.BooleanField(null=True)
-    notes = models.CharField(max_length=64, null=True)
+    notes = models.CharField(max_length=64, blank=True)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
